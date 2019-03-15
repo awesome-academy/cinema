5fb96df5e1cf9676f13b5dfce6b642fbd71d773d
PhungThuc <phung.van.thuc@framgia.com>
2019-03-15 13:50:10 +0700

    create migration

---
+++ database/migrations/2019_02_28_080305_create_coupon_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCouponTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('coupon_code')->nullable();
+            $table->string('status')->nullable();
+            $table->string('type')->nullable();
+            $table->decimal('price', 6, 3)->nullable();
+            $table->integer('percent')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
