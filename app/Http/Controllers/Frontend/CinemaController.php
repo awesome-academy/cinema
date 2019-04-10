9129532eea5d49c5dc9dc63d8fd52434a89d166c
PhungThuc <phung.van.thuc@framgia.com>
2019-04-10 08:39:38 +0700

    create homepage

---
+++ app/Http/Controllers/Frontend/CinemaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\Contracts\StoreInterface;
+
+class CinemaController extends Controller
+{
+    /**
+     * @var StoreInterface
+     */
+    public $store;
+
+    /**
+     * CinemaController constructor.
+     * @param StoreInterface $store
+     */
+
+    public function __construct(StoreInterface $store)
+    {
+        $this->store = $store;
+    }
+
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function getAll()
+    {
+        $store = $this->store->all();
+
+        return view('layout.frontend.cinema-list', compact('store'));
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function getComingSoon()
+    {
+        return view('layout.frontend.coming-soon');
+    }
+
+    public function getContact()
+    {
+        return view('layout.frontend.contact');
+    }
+}
